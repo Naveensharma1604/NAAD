8c034c595a03520f0a55e7c30eef64a5c95ac461
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-06 17:02:29 +0530

    Initial commit

---
+++ src/com/company/task0/introduction/Integer.java
@@ -0,0 +1,10 @@
+package com.company.task0.introduction;
+import java.util.Scanner;
+public class Integer {
+    public static void main(String[] args){
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Enter an Integer ");
+        int obj= myobj.nextInt();
+        System.out.println("entered number is "+ obj);
+    }
+}
