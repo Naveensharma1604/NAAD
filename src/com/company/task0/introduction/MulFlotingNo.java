c8bd0bd00af1d2501c8dcf08e29a369ff3208a17
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-06 17:29:26 +0530

    Done

---
+++ src/com/company/task0/introduction/MulFlotingNo.java
@@ -0,0 +1,14 @@
+package com.company.task0.introduction;
+
+import java.util.Scanner;
+
+public class MulFlotingNo {
+    public static void main(String[] args) {
+        Scanner myobj= new Scanner(System.in);
+        System.out.println("Enter first number ");
+        float obj1= myobj.nextFloat();
+        System.out.println("Enter Second number ");
+        float obj2= myobj.nextFloat();
+        System.out.println("Product of Numbers = "+(obj1*obj2));
+    }
+}
