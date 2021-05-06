63472138568af63a75722d23181b6fdc942433f4
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-06 17:42:01 +0530

    Done

---
+++ src/com/company/task0/introduction/AsciiValueOfChar.java
@@ -0,0 +1,15 @@
+package com.company.task0.introduction;
+import java.util.Scanner;
+
+public class AsciiValueOfChar {
+    public static void main(String[] args) {
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Enter the character to find ASCII Value ");
+        char obj = myobj.next().charAt(0);
+       // int obj1 =(int)obj;
+        System.out.println("the ASCII value of input char is : "+(int)obj);
+
+
+
+    }
+}
