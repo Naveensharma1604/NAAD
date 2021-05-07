2884503ef93c420ca3d5295074d4a618c5fa0c29
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-07 16:09:14 +0530

    complete

---
+++ src/com/company/task0/decisionMakingAndLoop/fibonnaci.java
@@ -0,0 +1,18 @@
+package com.company.task0.decisionMakingAndLoop;
+import java.util.Scanner;
+public class fibonnaci {
+    public static void main(String[] args) {
+        int a=0,b=1, c=0;
+        System.out.println(a);
+        System.out.println(b);
+                for(int i=0;c<100;i++)
+                {
+                    c=a+b;
+                    System.out.println(c);
+                    a=b;
+                    b=c;
+
+
+                }
+    }
+}
