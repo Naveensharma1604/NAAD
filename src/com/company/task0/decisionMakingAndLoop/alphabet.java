f52bddf1a6cea3da62b85148bc0e87c657c1e134
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-07 16:11:50 +0530

    complete

---
+++ src/com/company/task0/decisionMakingAndLoop/alphabet.java
@@ -0,0 +1,10 @@
+package com.company.task0.decisionMakingAndLoop;
+
+public class alphabet {
+    public static void main(String[] args) {
+        for(int i= 65 ;i<=90;i++)
+        {
+            System.out.println((char)i);
+        }
+    }
+}
