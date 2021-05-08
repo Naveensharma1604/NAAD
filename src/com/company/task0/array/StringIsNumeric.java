fd125e13f9c3f481ccb6992e2701e8500e1b36cf
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-08 16:18:06 +0530

    complete

---
+++ src/com/company/task0/array/StringIsNumeric.java
@@ -0,0 +1,20 @@
+package com.company.task0.array;
+
+public class StringIsNumeric {
+    public static void main(String[] args) {
+        String s= "2w345";
+        int temp=1;
+                for(int i=0;i<s.length();i++) {
+                    if (!((int)s.charAt(i)>=48 && (int)s.charAt(i)<=57))
+                    {
+                         temp=0;
+                         break;
+
+                    }
+                }
+                if(temp>=1)
+                    System.out.println("String is numeric");
+                else
+                    System.out.println("string is not numeric");
+    }
+}
