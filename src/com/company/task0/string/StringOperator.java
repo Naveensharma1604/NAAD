69f215a50b9c1864605b0cf45f77c70b1610de03
Naveen sharma <naveen.sharma1604@gmail.com>
2021-05-08 16:46:13 +0530

    complete

---
+++ src/com/company/task0/string/StringOperator.java
@@ -0,0 +1,11 @@
+package com.company.task0.string;
+
+public class StringOperator {
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+    }
+}
